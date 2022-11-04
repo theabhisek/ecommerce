e91d4838e5437f845ab0a581921b73355a9b1e16
theabhisek <chouhanabhishek802@gmail.com>
2022-11-04 15:19:24 +0530

    first commit

---
+++ server/node.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
